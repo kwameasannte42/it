c5f3a5118804fdc73317f55319062fedec9d852c
kwameasannte42 <coolllyod@gmail.com>
2024-09-16 15:47:02 -0400

    accordion

--- js/script.js
+++ js/script.js
@@ -7,18 +7,28 @@ document.addEventListener('DOMContentLoaded', function () {
         ticketForm.addEventListener('submit', function (e) {
             e.preventDefault();
             
-            // For this example, I'll just show a success message.
-
             // Reset
             ticketForm.reset();
 
-            // success message
+            // Show success message
             successMessage.style.display = 'block';
 
             // Hide the success message after 5 seconds
             setTimeout(() => {
                 successMessage.style.display = 'none';
             }, 5000);
+        });
+    }
+
+    // Accordion Navigation Toggle
+    const navbarToggle = document.getElementById('navbarToggle');
+    const navbarItems = document.getElementById('navbarItems');
+
+    if (navbarToggle && navbarItems) {
+        navbarToggle.addEventListener('click', function () {
+            // Toggle the 'open' class for the accordion and the icon
+            navbarToggle.classList.toggle('open');
+            navbarItems.classList.toggle('open');
         });
     }
 });
